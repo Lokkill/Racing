5c85b44e48d23e260d1f2cfeac4f6eeffe69bc41
Andrey Kataev <corvolk@yandex.ru>
2020-12-24 19:07:12 +0300

    Completed home work

--- src/our/company/Car.java
+++ src/our/company/Car.java
@@ -1,9 +1,64 @@
 package our.company;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
-public class Car{
+public class Car implements Runnable{
+    private static int CARS_COUNT;
+    private static boolean hasWinner;
+    static {
+        CARS_COUNT = 0;
+    }
+    private Race race;
+    private int speed;
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public Car(Race race, int speed) {
+        this.race = race;
+        this.speed = speed;
+        CARS_COUNT++;
+        this.name = "Участник #" + CARS_COUNT;
+    }
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " готовится");
+            Thread.sleep(500 + (int)(Math.random() * 800));
+            System.out.println(this.name + " готов");
+            MainClass.cl.countDown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public void startRace() {
+
+        ExecutorService ex = Executors.newFixedThreadPool(1);
+        for (int i = 0; i < race.getStages().size(); i++) {
+            int finalI = i;
+            ex.execute(new Thread(() -> {
+                race.getStages().get(finalI).go(this);
+            }));
+
+        }
+        ex.shutdown();
+        new Thread(() -> {
+            while (true){
+                if (ex.isTerminated()){
+                    MainClass.cl.countDown();
+                    if (!hasWinner){
+                        System.out.println(this.name + " - WIN");
+                        hasWinner = true;
+                    }
+                    break;
+                }
+            }
+        }).start();
+
+    }
 }
